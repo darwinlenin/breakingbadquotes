d7a063cdc39ad2e09792107800cc1da8f752acea
ZoeGeop Technologies <zoegeop@gmail.com>
2021-02-22 08:16:34 -0500

    first commit of API search

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif, Arial, Helvetica, sans-serif, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+function App() {
+const consultarAPI = async() => {
+  const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  const frase = await api.json();
+  console.log(frase[0]);
+}
+
+  return (
+    <Contenedor>
+      <Boton
+        onClick = {() =>consultarAPI()}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
