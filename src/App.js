9c8a392860d988038294e7154088dfd649b52829
ZoeGeop Technologies <zoegeop@gmail.com>
2021-03-03 07:55:15 -0500

    api service hosted in heroku to serach random breakingbad quotes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+import Frase from './components/Frase';
 
 const Contenedor = styled.div`
   display: flex;
   align-items: center;
   padding-top: 5rem;
   flex-direction: column;
-`;
+` ;
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background: -webkit-linear-gradient(top left, #dd4a06 0%, #ebb810 40%, #d41010 100%);
   background-size: 300px;
-  font-family: Arial, Helvetica, sans-serif, Arial, Helvetica, sans-serif, sans-serif;
+  font-family:  Arial, Helvetica, sans-serif;
   color: #fff;
   margin-top: 3rem;
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
-const consultarAPI = async() => {
-  const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
-  const frase = await api.json();
-  console.log(frase[0]);
-}
+
+  // state de frases
+  const [frase, guardarFrase] = useState({});
+
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json()
+    guardarFrase(frase[0]);
+  }
+
+
+  // Cargar una frase
+  useEffect( () => {
+    consultarAPI()
+  }, []);
 
   return (
     <Contenedor>
+      <Frase
+        frase={frase}
+      />
+
       <Boton
-        onClick = {() =>consultarAPI()}
+        onClick={consultarAPI}
       >
         Obtener Frase
       </Boton>
